fed6e1b85e3450cee08613f14650673e1064f41b
HubbleZA <rladeira95@gmail.com>
2022-01-16 14:27:02 +0200

    Basic Edits

--- budget.py
+++ budget.py
@@ -1,7 +1,23 @@
 class Category:
 
+    def __init__(self, name):
+        self.name = name
 
+    def deposit(self, value, description=''):
+        self.value = value
+        self.description = description
 
+    def withdraw(self, value, description=''):
+        self.value = value
+        self.description = description
+
+    def get_balance(self):
+        self.balance = 3
+
+    def transfer(self, value, catagory):
+        self.value = value
+        self.catagory = catagory
 
 
 def create_spend_chart(categories):
+    return
