f011f9e64c06dbc69e1f240f41a54f5e78f1f389
HubbleZA <rladeira95@gmail.com>
2022-01-16 19:30:06 +0200

    Finishing up the functions

--- budget.py
+++ budget.py
@@ -2,21 +2,38 @@ class Category:
 
     def __init__(self, name):
         self.name = name
+        self.ledger = []
+        self.value = 0
 
     def deposit(self, value, description=''):
-        self.value = value
+        self.value += value
         self.description = description
+        self.ledger.append({"amount": value,"description": description})
 
     def withdraw(self, value, description=''):
-        self.value = value
-        self.description = description
+        if self.check_funds(value) == False:
+            return False
+        else:
+            self.value -= value
+            self.description = description
+            self.ledger.append({"amount": -value,"description": description})
+            return True
 
     def get_balance(self):
-        self.balance = 3
+        print(self.value)
 
     def transfer(self, value, catagory):
-        self.value = value
-        self.catagory = catagory
+        if self.check_funds(value) == False:
+            return False
+        else:
+            self.withdraw(value, "Transfer to " + str(catagory.name))
+            catagory.deposit(value, "Transfer from " + str(self.name))
+            return True
+
+    def check_funds(self, value):
+        if self.value < value:
+            return False
+
 
 
 def create_spend_chart(categories):
